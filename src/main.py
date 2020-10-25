9479702d993012d140ac93d84b7127f678b8e6a2
ShaneMatsushima <skm1901@iolani.org>
2020-10-24 15:11:48 -1000

    start implementation to gui

--- src/main.py
+++ src/main.py
@@ -1,10 +1,23 @@
 from parseFunc import *
+import tkinter as tk
 
-if __name__ == '__main__':
-    # graph_value_over_time('C:\\Dev\\WaterQualityDataVisualizer\\src\\csv\\result.csv', 'Temperature, water')
-    # graph_value_over_time('C:\\Dev\\WaterQualityDataVisualizer\\src\\csv\\result.csv', 'pH')
-
+def graph_arsenic():
     box_plot('C:\\Dev\\WaterQualityDataVisualizer\\src\\csv\\result.csv', 'Arsenic', 'ug/l')
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+graphButton = tk.Button(frame, text='Graph', command=graph_arsenic)
+graphButton.pack()
+
+root.mainloop()
+
+
+# if __name__ == '__main__':
+#     graph_value_over_time('C:\\Dev\\WaterQualityDataVisualizer\\src\\csv\\result.csv', 'Temperature, water')
+#     graph_value_over_time('C:\\Dev\\WaterQualityDataVisualizer\\src\\csv\\result.csv', 'pH')
+
+#     box_plot('C:\\Dev\\WaterQualityDataVisualizer\\src\\csv\\result.csv', 'Arsenic', 'ug/l')
 
 
     
