1fab030e010d695c52a1939d31e5f0906181d94c
ShaneMatsushima <skm1901@iolani.org>
2020-10-25 10:08:46 -1000

    v0.1 implementation of gui

--- src/main.py
+++ src/main.py
@@ -1,16 +1,26 @@
 from parseFunc import *
-import tkinter as tk
+from tkinter import * 
+from tkinter.ttk import *
+from tkinter.filedialog import askopenfile 
 
-def graph_arsenic():
-    box_plot('C:\\Dev\\WaterQualityDataVisualizer\\src\\csv\\result.csv', 'Arsenic', 'ug/l')
+filename = ""
+  
+def open_file(): 
+    global filename
+    file = askopenfile(initialdir='/', filetypes =[('CSV Files', '*.csv')]) 
+    filename = file.name
+  
+root = Tk() 
+root.geometry('1000x600') 
+root.title('Water Quality Data Visualizer 1.0')
 
-root = tk.Tk()
-frame = tk.Frame(root)
-frame.pack()
-graphButton = tk.Button(frame, text='Graph', command=graph_arsenic)
-graphButton.pack()
+btn = Button(root, text ='Select CSV File', command = lambda:open_file()) 
+btn.place(relx=0.5, rely=0.5, anchor=CENTER) 
 
-root.mainloop()
+graph = Button(root, text='Plot data by time', command = lambda:box_plot(filename, 'Arsenic', 'ug/l'))
+graph.place(relx=0.5, rely=1.0, anchor=S)
+  
+mainloop() 
 
 
 # if __name__ == '__main__':
@@ -18,6 +28,3 @@ root.mainloop()
 #     graph_value_over_time('C:\\Dev\\WaterQualityDataVisualizer\\src\\csv\\result.csv', 'pH')
 
 #     box_plot('C:\\Dev\\WaterQualityDataVisualizer\\src\\csv\\result.csv', 'Arsenic', 'ug/l')
-
-
-    
